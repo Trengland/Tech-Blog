df85f3126bba8962d63cdf822446096321ef3410
Trengland <englandvivial@gmail.com>
2023-05-04 18:32:12 -0400

    fixed sql connection - still working on the app itself (css)

--- models/index.js
+++ models/index.js
@@ -1,16 +1,16 @@
 const Sequelize = require('sequelize');
+
 require('dotenv').config();
 
-const sequelize = process.env.JAWSDB_URL
-  ? new Sequelize(process.env.JAWSDB_URL)
-  : new Sequelize(
+const sequelize = new Sequelize(
       process.env.DB_NAME,
       process.env.DB_USER,
       process.env.DB_PASSWORD,
       {
+
         host: 'localhost',
         dialect: 'mysql',
-        port: 3306,
+        port: 3306
       }
     );
 
@@ -19,13 +19,14 @@ const db = {};
 db.Sequelize = Sequelize;
 db.sequelize = sequelize;
 
-db.User = require('./User')(sequelize, Sequelize);
-db.Blogpost = require('./Blogpost')(sequelize, Sequelize);
-db.Comment = require('./Comment')(sequelize, Sequelize);
+db.User = require('./User') , (sequelize, Sequelize);
+db.Blogpost = require('./Blogpost'),  (sequelize, Sequelize);
+db.Comment = require('./Comment'),  (sequelize, Sequelize);
 
 db.User.hasMany(db.Blogpost, {
   foreignKey: 'user_id',
 });
+
 
 db.Blogpost.belongsTo(db.User, {
   foreignKey: 'user_id',
