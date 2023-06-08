a63dc672388d4426a63bd48d5335e5d365fbde27
Trengland <englandvivial@gmail.com>
2023-06-08 14:19:31 -0400

    css stylesheet created and finished

--- server.js
+++ server.js
@@ -1,47 +1,46 @@
-// this will 'connect' everytihng - this file needs a lot of const's and middleware callouts to make the app connnected correctly - front and back
-const path = require('path')
-const express = require('express')
-const session = require('express-session')
-const exphbs = require('express-handlebars')
-// initialize express app to give handlbars access to all helper functions
-const helpers = require('./utils/helpers')
-// instantiate our express app
-const app = express()
-const PORT = process.env.PORT || 3306
-//bring in our connection
-const sequelize = require('./config/connection')
+const path = require('path');
+const express = require('express');
+const session = require('express-session');
+const exphbs = require('express-handlebars');
+const helpers = require('./utils/helpers');
+const app = express();
+const PORT = process.env.PORT || 3000;
+const sequelize = require('./config/connection');
+const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
-const SequelizeStore = require('connect-session-sequelize')(session.Store)
-
-//define our session
 const sess = {
-    secret: 'My Secret',
-    cookie: {
-        maxAge: 300000,
-        httpOnly: true,
-        secure: false,
-        sameSite: 'strict'
-    },
-    resave: false,
-    saveUninitialized: true,
-    store: new SequelizeStore({
-        db: sequelize
-    })
-}
+  secret: 'My Secret',
+  cookie: {
+    maxAge: 300000,
+    httpOnly: true,
+    secure: false,
+    sameSite: 'strict'
+  },
+  resave: false,
+  saveUninitialized: true,
+  store: new SequelizeStore({
+    db: sequelize
+  })
+};
 
-//applying all middleware - passing our helper functions to it
-app.use(session(sess))
-const hbs = exphbs.create({helpers})
-app.engine('handlebars', hbs.engine)
-app.set('view engine', 'handlebars')
-app.use(express.json())
-app.use(express.urlencoded({extended: false}))
-//specifying this app will run through the public directory
-app.use(express.static(path.join(__dirname,'public')))
-app.use(require('./controllers/'))
+app.use(session(sess));
+const hbs = exphbs.create({ helpers });
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
 
-//get port to start listening and sync our db to this app
-app.listen(PORT,() => {
-    console.log(`App Listening on PORT! ${PORT}`)
-    sequelize.sync({force: false})
-})
+app.use((req, res, next) => {
+  if (req.url.endsWith('.css')) {
+    res.setHeader('Content-Type', 'text/css');
+  }
+  next();
+});
+
+app.use(require('./controllers/'));
+
+app.listen(PORT, () => {
+  console.log(`App Listening on PORT! ${PORT}`);
+  sequelize.sync({ force: false });
+});
